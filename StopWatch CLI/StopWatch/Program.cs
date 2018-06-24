66b9917d3747c82948f304569941a05f0a26b035
Michael Klingelberg <Mike@Michaels-MacBook-Air.local>
2018-06-23 18:55:03 -0700

    working CLI app

---
+++ StopWatch CLI/StopWatch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            stopWatch.Start();
+            stopWatch.Stop();
+            Console.WriteLine("The total elapsed time is: " + stopWatch.GetDifference());
+        }
+    }
+}
---
+++ StopWatch CLI/StopWatch/StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        // fields
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isRunning;
+
+        public void Start()
+        {
+            if (!_isRunning)
+            {
+                Console.WriteLine("Please type start to begin the Stop Watch");
+                var input = Console.ReadLine().ToLower();
+
+                _startTime = DateTime.Now;
+                _isRunning = true;
+                Console.WriteLine(_startTime);
+            }
+            else 
+            {
+                throw new InvalidOperationException("Stopwatch is already running!");
+            }
+        } 
+
+        public void Stop()
+        {
+            if (_isRunning)
+            {
+                Console.WriteLine("Please type start to begin the Stop Watch");
+                var input = Console.ReadLine().ToLower();
+
+                _stopTime = DateTime.Now;
+                _isRunning = false;
+                Console.WriteLine(_stopTime);
+            }
+        }
+
+        public TimeSpan GetDifference()
+        {
+            var difference = _stopTime - _startTime;
+
+            return difference;
+        }
+    }
+}
